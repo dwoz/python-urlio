83bbec11911e1025b64643cf3af9a45df6750242
Daniel Wozniak <dan@orvant.com>
2014-08-01 08:20:16 -0700

    Factor out commonality

--- lib/config.py
+++ lib/config.py
@@ -8,6 +8,19 @@ __all__ = [
 
 def load_config(config_file):
     """
+    Parse the given config file location and return a dictionary. In
+    addition, if the conifg file has an option 'credentials_file' in the
+    'general' section, the file will be loaded and parsed.
+    """
+    with open(config_file) as fp:
+        config = parse_config(fp)
+    if 'credentials_file' in config:
+        with open(config['credentials_file']) as fp:
+            config.update(parse_config(fp, 'credentials'))
+    return config
+
+def parse_config(fp, section='general'):
+    """
     Load values from a configuration file into a dictionary. Items in
     the 'general' section of the config file will be placed as key,
     value pairs in the dictionary. Values of other sections get a key in
@@ -15,8 +28,7 @@ def load_config(config_file):
     that section.
     """
     parser = ConfigParser.ConfigParser()
-    with open(config_file) as fp:
-        parser.readfp(fp)
+    parser.readfp(fp)
     config = {}
     for section in parser.sections():
         for k, v in parser.items(section):
@@ -27,20 +39,4 @@ def load_config(config_file):
                 config[section] = {k: v}
             else:
                 config[section][k] = v
-    parser = ConfigParser.ConfigParser()
-    if 'credentials_file' in config:
-        with open(config['credentials_file']) as fp:
-            parser.readfp(fp)
-        for section in parser.sections():
-            for k, v in parser.items(section):
-                if section == 'credentials':
-                    if k in config:
-                        raise Exception(
-                            "Key {} exists in config".format(k)
-                        )
-                    config[k] = v
-                elif section not in config:
-                    config[section] = {k: v}
-                else:
-                    config[section][k] = v
     return config
