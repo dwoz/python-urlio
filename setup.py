76d93bc26ba37909096e825e741e3059564e105f
Daniel Wozniak <dan@orvant.com>
2015-04-22 21:36:29 -0700

    Handle dependecnies installed from source

--- setup.py
+++ setup.py
@@ -4,7 +4,18 @@ from distutils.core import setup
 from traxcommon import VERSION
 
 with open('requirements.txt') as fp:
-    install_requires = [s.strip() for s in fp.readlines()]
+    install_requires = []
+    for s in fp.readlines():
+        s = s.strip()
+        if s.startswith('-e'):
+            l = s.split('#')[1].split('&')
+            for a in l:
+                if a.startswith('egg'):
+                    b = a.lstrip('egg=')
+        else:
+            b = s
+        install_requires.append(b)
+
 setup(
     name='traxcommon',
     version='.'.join([str(i) for i in VERSION]),
