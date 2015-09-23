5dc4f317b5dd43f42d77765ed58c761c2871600f
Daniel Wozniak <dan@orvant.com>
2015-09-23 16:45:20 -0700

    Initial commit of edifact parser

--- traxcommon/test/test_parse.py
+++ traxcommon/test/test_parse.py
@@ -1,4 +1,5 @@
-from ..parse import xml_to_json
+from . import data_path
+from ..parse import xml_to_json, EdifactParser
 
 
 def test_xmltojson():
@@ -16,3 +17,12 @@ def test_xmltojson_attrs():
     """
     i = xml_to_json('<a><b Foo="Bar">1</b><b>2</b><b>x</b></a>')
     assert i == '{"a": {"b": [{"@foo": "Bar", "#text": 1}, {"#text": 2}, {"#text": "x"}]}}', i
+
+
+def test_edifact_parsing():
+    a = list(EdifactParser(filename=data_path('edifact'), split_elements=True))
+    assert a[1][2] == ['SC1COS']
+    assert a[1][3] == ['TRAX.HPENTEMEA']
+
+def test_multi_edifact():
+    pass
