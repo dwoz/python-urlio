97c75c56fe63aa9424c5ad128e30cb42b848cf9c
Daniel Wozniak <daniel.wozniak@traxtech.com>
2017-02-09 19:57:51 +0000

    Patch version bump

--- urlio/__init__.py
+++ urlio/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from .path import PathFactory, set_smb_username, set_smb_password
 from .url import UrlFactory
 
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 Url = UrlFactory()
 Path = PathFactory()
